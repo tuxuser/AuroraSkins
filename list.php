017b94edaabd0bfa048d2ea5a9bdd3d92dbe1f9a
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-03-22 04:45:43 +0100

    Initial commit

---
+++ list.php
@@ -0,0 +1,197 @@
+{
+  "backgrounds": [
+    {
+      "id": "bg.jveda.nxe.Aura_Green2",
+      "hash": "45D481B3B8BD15DB78A622C2B4F1627D",
+      "nombre": "Aura Green2",
+      "autor": "jveda",
+      "archivo": "Aura_Green2.jpg",
+      "version": "1",
+      "size": 54297
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Kinect",
+      "hash": "45D481B3B8BD15DB78A622C2B4F1627D",
+      "nombre": "NXE Kinect",
+      "autor": "jveda",
+      "archivo": "NXE_Kinect.jpg",
+      "version": "1",
+      "size": 54297
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Blue",
+      "hash": "32E018A8A00E78E93B32096937B2E9ED",
+      "nombre": "NXE Blue",
+      "autor": "jveda",
+      "archivo": "NXE_Blue.jpg",
+      "version": "1",
+      "size": 88223
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Blurple",
+      "hash": "02FC3BE391A72BAF2AC727D463377782",
+      "nombre": "NXE Blurple",
+      "autor": "jveda",
+      "archivo": "NXE_Blurple.jpg",
+      "version": "1",
+      "size": 247681
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_OOBE",
+      "hash": "60947360EC2ECF6CB4A65D774C6BD626",
+      "nombre": "Aura OOBE",
+      "autor": "jveda",
+      "archivo": "Aura_OOBE.jpg",
+      "version": "1",
+      "size": 68098
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_Green",
+      "hash": "95309D507B70EF13B4AC5A0A44728DA6",
+      "nombre": "Aura Green",
+      "autor": "jveda",
+      "archivo": "Aura_Green.jpg",
+      "version": "1",
+      "size": 48811
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_Blue",
+      "hash": "74F611619C674612FBD797B92398920A",
+      "nombre": "Aura Blue",
+      "autor": "jveda",
+      "archivo": "Aura_Blue.jpg",
+      "version": "1",
+      "size": 62843
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_Gray2",
+      "hash": "9CF400DC955FA74162946298F3F161A2",
+      "nombre": "Aura Gray2",
+      "autor": "jveda",
+      "archivo": "Aura_Gray2.jpg",
+      "version": "1",
+      "size": 47100
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Gradient",
+      "hash": "3F444CC9F99D95A6F785839E97921693",
+      "nombre": "NXE Gradient",
+      "autor": "jveda",
+      "archivo": "NXE_Gradient.jpg",
+      "version": "1",
+      "size": 33773
+    },
+    {
+      "id": "bg.jveda.nxe.Background",
+      "hash": "05B45585485FFD776063455A3AE14F21",
+      "nombre": "Background",
+      "autor": "jveda",
+      "archivo": "Background.jpg",
+      "version": "1",
+      "size": 24706
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_OOBE",
+      "hash": "F5DE5323FC5368F262836D1D2AB228FC",
+      "nombre": "NXE OOBE",
+      "autor": "jveda",
+      "archivo": "NXE_OOBE.jpg",
+      "version": "1",
+      "size": 44070
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Purple2",
+      "hash": "E2E71DFC6DC08A82E5F25CE0BC412471",
+      "nombre": "NXE Purple2",
+      "autor": "jveda",
+      "archivo": "NXE_Purple2.jpg",
+      "version": "1",
+      "size": 83191
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Red2",
+      "hash": "FF2EDD44F45BECD5680F7401329B01D8",
+      "nombre": "NXE Red2",
+      "autor": "jveda",
+      "archivo": "NXE_Red2.jpg",
+      "version": "1",
+      "size": 57504
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Night",
+      "hash": "43BFA54B5CC0C7C2ECCA4FE251A29839",
+      "nombre": "NXE Night",
+      "autor": "jveda",
+      "archivo": "NXE_Night.jpg",
+      "version": "1",
+      "size": 118365
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_Gray",
+      "hash": "145704C087A2A016EFE753F74E1F9422",
+      "nombre": "Aura Gray",
+      "autor": "jveda",
+      "archivo": "Aura_Gray.jpg",
+      "version": "1",
+      "size": 124018
+    },
+    {
+      "id": "bg.jveda.nxe.Aura_Purple",
+      "hash": "8900F918FE1AF6F1EF509FDF7B6C1591",
+      "nombre": "Aura Purple",
+      "autor": "jveda",
+      "archivo": "Aura_Purple.jpg",
+      "version": "1",
+      "size": 416060
+    },
+    {
+      "id": "bg.jveda.nxe.NXE_Green2",
+      "hash": "E99D25FA7D274DD2CA7ED0B25F9DEBB4",
+      "nombre": "NXE Green2",
+      "autor": "jveda",
+      "archivo": "NXE_Green2.jpg",
+      "version": "1",
+      "size": 74546
+    }
+  ],
+  "coverflows": [
+    {
+      "id": "cf.s0da.grapesoda",
+      "hash": "240AAE413986C9F051216556CDE6E274",
+      "nombre": "GrapeSoda CF",
+      "autor": "s0da",
+      "archivo": "GrapeSodaCF by s0da.cfljson",
+      "version": "1",
+      "size": 40791
+    },
+    {
+      "id": "cf.jveda.AuroraNXE",
+      "hash": "78CA1A7F8B5D820E8AE08025390AF0FD",
+      "nombre": "AuroraNXE",
+      "autor": "jveda",
+      "archivo": "AuroraNXE by jveda.cfljson",
+      "version": "1",
+      "size": 40791
+    }
+  ],
+  "skins": [
+    {
+      "id": "skin.s0da.grapesoda",
+      "hash": "F1AF91767E0712EF489CBEFAC7F55915",
+      "nombre": "GrapeSoda",
+      "autor": "s0da",
+      "archivo": "skin.s0da.grapesoda.xzp",
+      "version": "10",
+      "size": 7651011
+    },
+    {
+      "id": "skin.jveda.AuroraNXE",
+      "hash": "3AC3F978EC7616B2DEF2FD088AA2BB5B",
+      "nombre": "AuroraNXE",
+      "autor": "jveda",
+      "archivo": "skin.jveda.AuroraNXE.xzp",
+      "version": "1",
+      "size": 11646057
+    }
+  ]
+}
